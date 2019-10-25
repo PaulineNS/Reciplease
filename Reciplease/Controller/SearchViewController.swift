e15647c0af560f39aeacc28876c58e9aa2f4e83b
Pauline <pauline.nomballais@hotmail.fr>
2019-10-25 12:15:01 +0200

    I managed the add of ingredient in tableView in SearchVC

---
+++ Reciplease/Controller/SearchViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SearchViewController.swift
+//  Reciplease
+//
+//  Created by Pauline Nomballais on 25/10/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    var ingredientsArray = [String]()
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var addIngredientButton: UIButton!
+    @IBOutlet weak var ingredientsTableView: UITableView!
+    @IBOutlet weak var searchRecipesButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ingredientsTableView.delegate = self
+        ingredientsTableView.dataSource = self
+    }
+    
+    
+    @IBAction func didTapButtonToAddIngredient(_ sender: Any) {
+        guard searchTextField.text?.isEmpty == false else {
+            presentAlert(message: "Please enter an ingredient :) ")
+            return
+        }
+        addIngredientToTableView()
+        ingredientsTableView.reloadData()
+    }
+    
+    func addIngredientToTableView() {
+        
+        guard let searchbarTxt = searchTextField.text else {
+            return
+        }
+        ingredientsArray.append(searchbarTxt)
+        searchTextField.text = " "
+        print("\(ingredientsArray)")
+    }
+}
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredientsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = ingredientsArray[indexPath.row]
+        
+        return cell
+    }
+}
+
---
+++ Reciplease/ControllerExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ControllerExtension.swift
+//  Reciplease
+//
+//  Created by Pauline Nomballais on 25/10/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import UIKit
+
+//MARK: ALERT
+extension UIViewController {
+    
+    func presentAlert(message: String){
+        let alertVC = UIAlertController(title: "Erreur!", message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        return present(alertVC, animated: true, completion: nil)
+    }
+    
+}
