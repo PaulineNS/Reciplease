ac4731703ebe8df5b46659c1e9df0cb5785eed11
Pauline <pauline.nomballais@hotmail.fr>
2019-11-25 13:01:47 +0100

    Creation of RecipeDetails Controller

--- Reciplease/Recipes/View/RecipeTableViewCell.swift
+++ Reciplease/Recipes/View/RecipeTableViewCell.swift
@@ -13,17 +13,6 @@ class RecipeTableViewCell: UITableViewCell {
     @IBOutlet weak var recipeImageView: UIImageView!
     @IBOutlet weak var recipeTitleLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 //    // Cell Configuration
     func configure(title: String, pictureUrl: URL){
         recipeTitleLabel.text = title
